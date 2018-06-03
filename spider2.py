ef794c7c70a95147736577764ddb8b79c3590883
adymilk <924114103@qq.com>
2018-06-03 14:22:35 +0800

    a

--- spider2.py
+++ spider2.py
@@ -4,16 +4,26 @@
 from urllib import request  
 from  bs4 import BeautifulSoup  
 import re  
-import time  
-      
-url = "https://www.zhihu.com/question/22918070"  
-html = request.urlopen(url).read().decode('utf-8')  
+import time
+import urllib
+import http  
+  
+url = "https://www.pexels.com/popular-photos"
+
+user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.44 Safari/537.36 OPR/24.0.1558.25 (Edition Next)'
+myheader={'User-Agent':user_agent,
+          'Host':'www.pexels.com',
+          'GET':url}
+
+
+req = urllib.request.Request(url, headers=myheader)
+html = urllib.request.urlopen(req).read().decode('utf-8')  
 soup = BeautifulSoup(html,'html.parser')  
-#print(soup.prettify())  
+# print(soup.prettify())
       
 #用Beautiful Soup结合正则表达式来提取包含所有图片链接（img标签中，class=**，以.jpg结尾的链接）的语句  
-links = soup.find_all('img', "origin_image zh-lightbox-thumb",src=re.compile(r'.jpg$'))  
-print(links)  
+links = soup.find_all('img', attrs={'class':'photo-item__img'})  
+# print(links)  
 #设置保存图片的路径，否则会保存到程序当前路径  
 #路径前的r是保持字符串原始值的意思，就是说不对其中的符号进行转义  
 path = r'./images/'
